0f0c9f723851680928cda0417ff038a34f879feb
Pablo Fernandez <PabloFernandezMila@gmail.com>
2022-04-01 19:23:07 -0300

    Added 404 logic to routers

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -2,26 +2,32 @@ const express = require("express");
 const usersRouter = express.Router();
 
 //Users
-const users = [{
+const users = [
+  {
     userID: 1,
     userName: "Gustavo",
     userLastname: "Rodriguez",
     email: "Sample@gmail.com",
     avatarURL: "https://cdn-icons-png.flaticon.com/512/147/147144.png",
-}, ];
+  },
+];
 
 //Users name end-point
 usersRouter.get("/:id/name", (request, response) => {
-    const userID = request.params.id;
-    let userToBeReturn = null;
-    users.forEach((user) => {
-        if (user.userID == userID) {
-            userToBeReturn = user;
-        }
-    });
-    userToBeReturn !== null ?
-        response.send(userToBeReturn.userName) :
-        response.status(404).send("User not found");
+  const userID = request.params.id;
+  let userToBeReturn = null;
+  users.forEach((user) => {
+    if (user.userID == userID) {
+      userToBeReturn = user;
+    }
+  });
+  userToBeReturn !== null
+    ? response.status(200).send({
+        userName: userToBeReturn.userName,
+      })
+    : response.status(404).send({
+        message: "User not found",
+      });
 });
 
 module.exports = usersRouter;
